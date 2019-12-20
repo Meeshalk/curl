ebf526cdc5a05ec56e163dddee96979f69eef833
Meeshalk <meeshal1994@gmail.com>
2019-12-20 10:02:33 +0530

    added my page3

--- app/index.php
+++ app/index.php
@@ -8,36 +8,39 @@
     <pre>
 
       <?php
-      require __DIR__ . '/../vendor/autoload.php';
-      function writeLog($s){
-        $log = fopen('acc_log.log', 'a+');
-        if($log != false){
-          fwrite($log, "[".date('Y-m-d H:i:s')."] ".$s.PHP_EOL);
+      set_time_limit(55)
+      for ($i=0; $i < 12; $i++) {
+        require __DIR__ . '/../vendor/autoload.php';
+        function writeLog($s){
+          $log = fopen('acc_log.log', 'a+');
+          if($log != false){
+            fwrite($log, "[".date('Y-m-d H:i:s')."] ".$s.PHP_EOL);
+          }
+          fclose($log);
         }
-        fclose($log);
-      }
 
-      use \Curl\Curl;
-      $curl = new Curl();
-      $url = "http://www.yad2.co.il/api/pre-load/getFeedIndex/vehicles/private-cars?page=1&compact-req=1&forceLdLoad=true";
-      $curl->setOpts([
-        CURLOPT_HEADER => TRUE,
-        CURLOPT_FOLLOWLOCATION => TRUE,
-        CURLOPT_RETURNTRANSFER => TRUE,
-        CURLOPT_CONNECTTIMEOUT => 30,
-        CURLOPT_TIMEOUT => 180,
-        CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'
-      ]);
-      $curl->get($url);
-      $cLenth = strlen($curl->response);
-      if($cLenth <= 35000)
-        $cType = "Blocked";
-      else
-        $cType = "Allowed";
-      if ($curl->error) {
-        writeLog('Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . " Content Length: ".$cLenth . "Content Type: ".$cType);
-      } else {
-        writeLog('Success: ' . $curl->getHttpStatusCode() . " :  Content Length: ".$cLenth . " Content Type: ".$cType);
+        use \Curl\Curl;
+        $curl = new Curl();
+        $url = "http://www.yad2.co.il/api/pre-load/getFeedIndex/vehicles/private-cars?page=1&compact-req=1&forceLdLoad=true";
+        $curl->setOpts([
+          CURLOPT_HEADER => TRUE,
+          CURLOPT_FOLLOWLOCATION => TRUE,
+          CURLOPT_RETURNTRANSFER => TRUE,
+          CURLOPT_CONNECTTIMEOUT => 30,
+          CURLOPT_TIMEOUT => 180,
+          CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36'
+        ]);
+        $curl->get($url);
+        $cLenth = strlen($curl->response);
+        if($cLenth <= 35000)
+          $cType = "Blocked";
+        else
+          $cType = "Allowed";
+        if ($curl->error) {
+          writeLog('Error: ' . $curl->errorCode . ': ' . $curl->errorMessage . " Content Length: ".$cLenth . " Content Type: ".$cType);
+        } else {
+          writeLog('Success: ' . $curl->getHttpStatusCode() . " :  Content Length: ".$cLenth . " Content Type: ".$cType);
+        }
       }
       ?>
 
